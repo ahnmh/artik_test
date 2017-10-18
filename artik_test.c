9d83b3796db891584606401fe27526f757b5d9df
ahnmh <hiro798@naver.com>
2017-10-18 15:22:04 +0900

    initial commit

---
+++ artik_test.c
@@ -0,0 +1,17 @@
+/*
+ * artik_test.c
+ *
+ *  Created on: 2017. 10. 18.
+ *      Author: myunghoon.ahn
+ */
+
+
+#include <stdio.h>
+#include "artik_test.h"
+
+int main()
+{
+	printf("hello artik!\n");
+
+	return 0;
+}
---
+++ artik_test.h
@@ -0,0 +1,13 @@
+/*
+ * artik_test.h
+ *
+ *  Created on: 2017. 10. 18.
+ *      Author: myunghoon.ahn
+ */
+
+#ifndef ARTIK_TEST_H_
+#define ARTIK_TEST_H_
+
+
+
+#endif /* ARTIK_TEST_H_ */
